eef63d8a73ea3b2dbe03a4805af451a77dbbe762
Robert Shepley <shepleysound@gmail.com>
2022-09-21 19:41:10 -0700

    Add methods for people route.

--- src/routes/people.js
+++ src/routes/people.js
@@ -9,11 +9,38 @@ router.get('/people', async (req, res) => {
   await res.status(200).json(people);
 });
 
+router.get('/people/:id', async (req, res) => {
+  const person = await People.findAll({
+    where: {
+      id: req.params.id,
+    },
+  });
+  res.status(200).json(person);
+});
+
 router.post('/people', async (req, res) => {
   console.log('Request Body:', req.body);
 
   const newPerson = await People.create(req.body);
-  await res.status(201).send(newPerson);
+  res.status(201).send(newPerson);
+});
+
+router.put('/people/:id', async (req, res) => {
+  const updated = await People.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  });
+  res.status(200).send(`${updated} row(s) updated successfully.`);
+});
+
+router.delete('/people/:id', async (req, res) => {
+  const deleted = await People.destroy({
+    where: {
+      id: req.params.id,
+    },
+  });
+  res.status(200).send(`${deleted} row(s) deleted successfully.`);
 });
 
 module.exports = router;
